332b7e7918d6e86bdbd989da09d1e2123648d758
tduncan14 <nbatre1@yahoo.com>
2023-10-31 23:48:41 -0700

    keep moving forward

--- app/(authenicate)/login.js
+++ app/(authenicate)/login.js
@@ -1,13 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View,SafeAreaView,Image
+} from 'react-native';
 import React from 'react';
 
 
 const login = () => {
 
     return(
-        <View>
-            <Text>login</Text>
-        </View>
+        <SafeAreaView style={{flex:1,backgroundColor:"White",alignItems:"center"}}>
+            <View>
+                <Image style={{width:150, height:100, resizeMode:"contain"}} source={{uri:"https://www.freepnglogos.com/uploads/linkedin-logo-transparent-png-25.png"}} />
+            </View>
+        </SafeAreaView>
     )
 }
 
