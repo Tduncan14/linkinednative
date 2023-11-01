56dc32437048059115fa3795226d5efa6a2a6dfe
tduncan14 <nbatre1@yahoo.com>
2023-10-31 17:39:01 -0700

    keep moving forward

---
+++ app/authenicate/Login.js
@@ -0,0 +1,20 @@
+import {Stylesheet, Text, View} from 'react-native';
+import React from 'react';
+
+
+const Login = () => {
+
+    return(
+        <View>
+            <Text>login</Text>
+        </View>
+    )
+}
+
+
+
+export default Login
+
+const styles = StyleSheet.create({
+    
+})
