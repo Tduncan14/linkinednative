e1eaf07c6b9659fa1db4cdfec9e52a02c015a280
tduncan14 <nbatre1@yahoo.com>
2023-11-02 17:44:39 -0700

    keep moving forward

--- app/(authenicate)/login.js
+++ app/(authenicate)/login.js
@@ -1,15 +1,48 @@
-import {StyleSheet, Text, View,SafeAreaView,Image
+import {
+    StyleSheet, Text, View, SafeAreaView, Image, KeyboardAvoidingView, TextInput
 } from 'react-native';
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'; 
 
 
 const login = () => {
 
-    return(
-        <SafeAreaView style={{flex:1,backgroundColor:"White",alignItems:"center"}}>
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "White", alignItems: "center" }}>
             <View>
-                <Image style={{width:150, height:100, resizeMode:"contain"}} source={{uri:"https://www.freepnglogos.com/uploads/linkedin-logo-transparent-png-25.png"}} />
+                <Image style={{ width: 150, height: 100, resizeMode: "contain" }} source={{ uri: "https://www.freepnglogos.com/uploads/linkedin-logo-transparent-png-25.png" }} />
             </View>
+
+            <KeyboardAvoidingView>
+                <View style={{ alignItems: "center" }}>
+                    <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 12, color: "#041E42" }}> Log in to your Account</Text>
+                </View>
+
+                <View style={{marginTop:70}}>
+                    <View style={{ flexDirection: "row", alignItems: "center",gap:5, backgroundColor: "#E0E0E0", paddingVertical: 5, borderRadius: 5, marginTop: 30 }}>
+                        <MaterialIcons style={{marginLeft:8}} name="email" size={24} color="black" />
+                        <TextInput style={{color:"gray",marginVertical:10,width:300
+                    }} placeholder="enter your Email" />
+                    </View>
+
+                    <View  style={{marginTop:2}}>
+                    <View style={{ flexDirection: "row", alignItems: "center",gap:5, backgroundColor: "#E0E0E0", paddingVertical: 5, borderRadius: 5, marginTop: 30 }}>
+                        <AntDesign  style={{marginLeft:8}} name="lock" size={24} color="black" />
+                        <TextInput secureTextEntry={true} style={{color:"gray",marginVertical:10,width:300
+                    }} placeholder="enter your password" />
+                    </View>
+                    </View>
+
+
+                    <View style={{marginTop:16,flexDirection:"row", alignItems:"center",justifyContent:"space-between"}}>
+                        <Text>Keep me logged in</Text>
+                        <Text>Forgot password</Text>
+                    </View>
+
+                
+                </View>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
@@ -19,5 +52,5 @@ const login = () => {
 export default login
 
 const styles = StyleSheet.create({
-    
+
 })
