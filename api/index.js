d964423a1bf1f4598e6d656ff080e5185295d229
tduncan14 <nbatre1@yahoo.com>
2023-11-02 18:58:07 -0700

    keep moving forward

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
+const User = require('./models/user');
+const Post = require('./models/post');
 require('dotenv').config()
 
 
@@ -21,6 +23,83 @@ mongoose.connect(process.env.MONGOURI,{useNewUrlParser:true, useUnifiedTopology:
     console.log('is connected to the database mongo');
 })
 .catch(e => console.log(`this is the error${e}`))
+
+
+
+// endpoint to register user in the backend
+
+
+
+app.post("/register", async(req,res) => {
+
+    try{
+        const{name,email,profileImage,password} = req.body
+
+        //check if the email exist
+
+        const existingUser = await User.findOne({email});
+
+
+        if(existUser){
+            console.log('email already taken')
+            res.status(400).json({message:"email already registered"})
+        }
+
+
+        // create a new user
+
+        const newUser = new User({
+            name,
+            email,
+            password,
+            profileImage
+        });
+
+        // generate a verification token
+
+        newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+
+
+        // save the user to the database
+
+        await newUser.save()
+
+        // this the verfication email to the user
+
+
+        sendVerificationEmail(newUser.email,newUser.verificationToken)
+
+        res.status(202).json({message:"Registration successful, check your email to verify your account"})
+
+    }
+
+    catch(err){
+        console.log('this erroring resigter user','err')
+        res.status(500).json({message:"Registration failure"})
+    }
+})
+
+
+// to send the email
+
+const sendVerificationEmail = async(email,verificationToken) =>{
+
+    // creating the nodemailer
+
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:"windbreeze12@gmail.com",
+            pass:process.env.PASSCODE
+        }
+    })
+
+
+
+
+
+}
 
 
 app.listen(port,()=>{
